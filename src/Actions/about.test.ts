017dce1e078d2f00eb922386c2df7b2a8c312ca6
Rich Plastow <rich.plastow@easeltv.com>
2020-08-29 10:39:24 +0100

    Adds unit tests for the `about` action

---
+++ src/Actions/about.test.ts
@@ -0,0 +1,51 @@
+import { ActionContextI, ActionI } from "coanmo-plugin-cli";
+import { about } from "./about";
+const { fn } = about;
+
+// Mocks.
+
+const mockContext: ActionContextI = {
+  $stdout: null,
+  actions: [],
+  doc: {} as Document,
+  meta: "Mock-Meta",
+  name: "Mock-About",
+  setStore() {
+    return;
+  },
+  store: {},
+  version: "1.2.3"
+};
+
+// Public functions.
+
+describe("about.fn()", () => {
+  it("returns the expected string when passed no arguments", () => {
+    expect(fn([], mockContext)).toContain(
+      "These instructions explain how to:\n  1. ");
+  });
+  it("returns as expected when passed '1'", () => {
+    expect(fn(["1"], mockContext)).toContain(
+      "1. Create a new Google Cloud account\n\n");
+  });
+  it("returns an error when passed '0'", () => {
+    expect(fn(["0"], mockContext)).toBe(
+      "ERROR: 'about' expected a positive integer, but got '0'"
+    );
+  });
+  it("returns an error when passed 'not-a-number'", () => {
+    expect(fn(["not-a-number"], mockContext)).toBe(
+      "ERROR: 'about' expected a positive integer, but got 'not-a-number'"
+    );
+  });
+  it("returns an error when passed '9999'", () => {
+    expect(fn(["9999"], mockContext)).toBe(
+      "ERROR: 'about' has no section '9999' — try '1'"
+    );
+  });
+  it("returns an error when passed two arguments", () => {
+    expect(fn(["1", "2"], mockContext)).toBe(
+      "ERROR: 'about' expected 0 or 1 args, but got 2"
+    );
+  });
+});
--- src/Actions/about.ts
+++ src/Actions/about.ts
@@ -50,34 +50,33 @@ const sections: SectionI[] = [
     heading: "Remove ‘things’ with a given title",
     main: `
 @TODO write these instructions`
-  },
+  }
 ];
 
 export const about: ActionI = {
   name: "about",
   summary: "Step-by-step instructions",
-  synopsis:
-`To show all step-by-step instructions:
+  synopsis: `To show all step-by-step instructions:
   > about
 To show just the third section:
   > about 3`,
   fn(args: string[]) {
     if (args.length === 0)
-      return `These instructions explain how to:\n${
-        sections.map( ({heading}, index) =>
-          `  ${index + 1}. ${heading}` ).join("\n")
-      }\n${
-        sections.map( ({heading, main}, index) =>
-        `\n${"-".repeat(40)}\n${index + 1}: ${heading}\n${main}` ).join("\n")
-      }`;
+      return `These instructions explain how to:\n${sections
+        .map(({ heading }, index) => `  ${index + 1}. ${heading}`)
+        .join("\n")}\n${sections
+        .map(
+          ({ heading, main }, index) =>
+            `\n${"-".repeat(40)}\n${index + 1}: ${heading}\n${main}`
+        )
+        .join("\n")}`;
     if (args.length !== 1)
       return `ERROR: 'about' expected 0 or 1 args, but got ${args.length}`;
-    if (args[0][0] === '0' || ! /^\d+$/.test(args[0]))
+    if (args[0][0] === "0" || !/^\d+$/.test(args[0]))
       return `ERROR: 'about' expected a positive integer, but got '${args[0]}'`;
-    const sectionIndex = (+args[0]) - 1;
+    const sectionIndex = +args[0] - 1;
     const section = sections[sectionIndex];
-    if (!section)
-      return `ERROR: 'about' has no section '${args[0]}' — try '1'`;
+    if (!section) return `ERROR: 'about' has no section '${args[0]}' — try '1'`;
     return `${sectionIndex + 1}. ${section.heading}\n${section.main}`;
   }
 };
